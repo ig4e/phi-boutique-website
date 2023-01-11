230955cc3ad3dbb60ae29c8dfec04c3a5cfbbe1c
Sekai <ahmedx793@gmail.com>
2023-01-11 11:07:30 +0200

    fix the main div not taking 100% height

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
 	return (
 		<div
-			className="text-netural font-custom max-w-lg mx-auto bg-white"
+			className="text-netural font-custom max-w-lg mx-auto bg-white min-h-screen"
 			dir={locale === "ar" ? "rtl" : "ltr"}
 		>
 			<Navbar></Navbar>
